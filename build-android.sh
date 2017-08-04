63c034c59d71067d745e82f70e5f24e1b45646d9
tianbei <tianbei@meituan.com>
2017-08-04 15:16:12 +0800

    build LTLIBRARY

--- build-android.sh
+++ build-android.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
- 
+
+NDK_HOME=/Users/tianbei/.localapps/android-ndk-r12b
+PREFIX=/Users/tianbei/hello 
+
 set -e
 set -x
 
 cd `dirname $0`
 pwd
 autoreconf -i
-NDK_HOME=/Users/tianbei/.localapps/android-ndk-r12b
 NDK_BIN=$NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin
 export CC="$NDK_BIN/arm-linux-androideabi-gcc --sysroot=$NDK_HOME/platforms/android-12/arch-arm/"
 export AR="$NDK_BIN/arm-linux-androideabi-ar"
@@ -16,5 +18,5 @@ export CFLAGS="-fPIC -ggdb -mfpu=vfp -mfloat-abi=softfp -std=c99 -pie -fPIE"
 export CPPFLAGS="-fPIC -ggdb -mfpu=vfp -mfloat-abi=softfp -pie -fPIE"
 export LIBS="-lc -lgcc"
 
-./configure -host=x86_64 --prefix=/Users/tianbei/hello
+./configure -host=x86_64 --prefix=$PREFIX
 make -s
